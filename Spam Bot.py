832ff57afd47bf734b37068304b5d67c6389a8a4
skiftyy <77881859+skiftyy@users.noreply.github.com>
2021-03-23 11:15:07 +1000

    Texts + Application Presets
    You can now choose which application to use the code on and the code will adjust to run smoothly on that app.
    
    Some new scripts have been added too including "Cars 2"!

--- Spam Bot.py
+++ Spam Bot.py
@@ -2,7 +2,7 @@ import pyautogui, time
 
 #Presets Simple Letter by Letter
 #O_O = "Oh.yeah.gamers"
-j = "Alistair.Nicol.is.a.beautiful.human.being"
+j = "Lord.Fisher.is.a.beautiful.human.being"
 k = ""
 l = ""
 m = ""
@@ -18,16 +18,26 @@ e = []
 
 #Presets Advance
 #Change path for different scripts
-bee = open("C:\\Users\\Scott Kift\\Desktop\\Code\\Spam Bot\\Scripts\\beemovie.txt", 'r')
-gangnam = open("C:\\Users\\Scott Kift\\Desktop\\Code\\Spam Bot\\Scripts\\gangnam.txt", 'r')
-pra = open("C:\\Users\\Scott Kift\\Desktop\\Code\\Spam Bot\\Scripts\\PRA.txt", 'r')
-x = open("C:\\Users\\Scott Kift\\Desktop\\Code\\Spam Bot\\Scripts\\PRA.txt", 'r')
-y = open("C:\\Users\\Scott Kift\\Desktop\\Code\\Spam Bot\\Scripts\\PRA.txt", 'r')
-z = open("C:\\Users\\Scott Kift\\Desktop\\Code\\Spam Bot\\Scripts\\PRA.txt", 'r')
+bee = open("C:\\Users\\26495\\OneDrive - Brisbane Boys' College\\School\\Coding\\GitHub Repository\\SpamBot\\Scripts\\beemovie.txt", 'r')
+gangnam = open("C:\\Users\\26495\OneDrive - Brisbane Boys' College\School\Coding\GitHub Repository\SpamBot\\Scripts\\gangnam.txt", 'r')
+pra = open("C:\\Users\\26495\OneDrive - Brisbane Boys' College\School\Coding\GitHub Repository\SpamBot\Scripts\\PRA.txt", 'r')
+cars = open("C:\\Users\\26495\OneDrive - Brisbane Boys' College\School\Coding\GitHub Repository\SpamBot\Scripts\\Cars 2.txt", 'r')
+y = open("C:\\Users\\26495\OneDrive - Brisbane Boys' College\School\Coding\GitHub Repository\SpamBot\Scripts\\PRA.txt", 'r')
+z = open("C:\\Users\\26495\OneDrive - Brisbane Boys' College\School\Coding\GitHub Repository\SpamBot\Scripts\\PRA.txt", 'r')
+
+#Application
+teams = 0.5
+xbox = 0.5
+winword = 0
+whatsapp = 0
+instagram = 0
+discord = 1
 
 #Spammer
 time.sleep(5)
 #Change variable to chosen option
 for word in gangnam:
     pyautogui.typewrite(word)
-    pyautogui.press("enter")
+    time.sleep(teams)
+    #pyautogui.press("enter")
+pyautogui.press("enter")
