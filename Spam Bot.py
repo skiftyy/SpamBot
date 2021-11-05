d9cc055674072e2402433272cddaca17198f2b7e
skiftyy <77881859+skiftyy@users.noreply.github.com>
2021-11-05 14:47:57 +1000

    Fixed File Location
    File locations have been fixed now

--- Spam Bot.py
+++ Spam Bot.py
@@ -2,12 +2,12 @@ import pyautogui, time
 
 #Presets Advance
 #Change path for different scripts
-bee = open("C:\\Users\\26495\\OneDrive - Brisbane Boys' College\\School\\Coding\\GitHub Repository\\SpamBot\\Scripts\\beemovie.txt", 'r')
-gangnam = open("C:\\Users\\26495\OneDrive - Brisbane Boys' College\School\Coding\GitHub Repository\SpamBot\\Scripts\\gangnam.txt", 'r')
-pra = open("C:\\Users\\26495\OneDrive - Brisbane Boys' College\School\Coding\GitHub Repository\SpamBot\Scripts\\PRA.txt", 'r')
-cars = open("C:\\Users\\26495\OneDrive - Brisbane Boys' College\School\Coding\GitHub Repository\SpamBot\Scripts\\Cars 2.txt", 'r')
-y = open("C:\\Users\\26495\OneDrive - Brisbane Boys' College\School\Coding\GitHub Repository\SpamBot\Scripts\\PRA.txt", 'r')
-z = open("C:\\Users\\26495\OneDrive - Brisbane Boys' College\School\Coding\GitHub Repository\SpamBot\Scripts\\PRA.txt", 'r')
+bee = open("C:\\GitHub\\SpamBot\\Scripts\\beemovie.txt", 'r')
+gangnam = open("C:\\GitHub\\SpamBot\\Scripts\\Gangnam.txt", 'r')
+pra = open("C:\\GitHub\\SpamBot\\Scripts\\PRA.txt", 'r')
+cars = open("C:\\GitHub\\SpamBot\\Scripts\\Cars 2.txt", 'r')
+y = open("C:\\GitHub\\SpamBot\\Scripts\\beemovie.txt", 'r')
+z = open("C:\\GitHub\\SpamBot\\Scripts\\beemovie.txt", 'r')
 
 #Application
 teams = 0.5
@@ -20,7 +20,7 @@ discord = 1
 #Spammer
 time.sleep(5)
 #Change variable to chosen option
-for word in cars:
+for word in gangnam:
     pyautogui.typewrite(word)
     time.sleep(winword)
     #pyautogui.press("enter")
