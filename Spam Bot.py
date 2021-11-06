9782612d26d79a524ee403eb9accaa74264628ec
skiftyy <srk0309@yahoo.com.au>
2021-11-06 10:02:47 +1000

    Interactive Console
    The spam bot now needs no code editing to work and gathers terminal inputs to set up

--- Spam Bot.py
+++ Spam Bot.py
@@ -1,6 +1,5 @@
 import pyautogui, time
 
-#Presets Advance
 #Change path for different scripts
 bee = open("C:\\GitHub\\SpamBot\\Scripts\\beemovie.txt", 'r')
 gangnam = open("C:\\GitHub\\SpamBot\\Scripts\\Gangnam.txt", 'r')
@@ -9,19 +8,72 @@ cars = open("C:\\GitHub\\SpamBot\\Scripts\\Cars 2.txt", 'r')
 y = open("C:\\GitHub\\SpamBot\\Scripts\\beemovie.txt", 'r')
 z = open("C:\\GitHub\\SpamBot\\Scripts\\beemovie.txt", 'r')
 
-#Application
-teams = 0.5
-xbox = 0.5
+#Applications
 winword = 0
+discord = 1
+teams = 0.5
 whatsapp = 0
 instagram = 0
-discord = 1
 
-#Spammer
+#Variables
+script = gangnam
+app = winword
+start = 0
+start2 = 0
+
+while start == 0:
+    print("SCRIPT OPTIONS:")
+    print("bee")
+    print("gangnam")
+    print("pra")
+    print("cars")
+    print("")
+    a = input("Script: ")
+    if a == "bee":
+        script = bee
+        start = 1
+    elif a == "gangnam":
+        script = gangnam
+        start = 1
+    elif a == "pra":
+        script = pra
+        start = 1
+    elif a == "cars":
+        script = cars
+        start = 1
+    else:
+        start = 0
+
+while start2 == 0:
+    print("")
+    print("APP OPTIONS:")
+    print("word")
+    print("discord")
+    print("instagram")
+    print("teams")
+    print("whatsapp")
+    b = input("App: ")
+    if b == "word":
+        app = winword
+        start2 = 1
+    elif b == "discord":
+        app = discord
+        start2 = 1
+    elif b == "teams":
+        app = teams
+        start2 = 1
+    elif b == "whatsapp":
+        app = whatsapp
+        start2 = 1
+    elif b == "instagram":
+        app = instagram
+        start2 = 1
+    else:
+        start2 = 0
+        
+
 time.sleep(5)
-#Change variable to chosen option
-for word in gangnam:
+for word in script:
     pyautogui.typewrite(word)
-    time.sleep(winword)
-    #pyautogui.press("enter")
+    time.sleep(app)
 pyautogui.press("enter")
